db56df82e4c55d89ec32a182dc093e7d7ac81bd1
Tetiana Yakovchuk <tanyalife294@gmail.com>
2020-10-22 17:57:40 +0300

    Implemented functionality of getData, setData

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //_______________ Local Storage ____
-const obj = {
-  step1: ['Hit the gym', 'Pay bills'],
+
+// let itemsArray = localStorage.getItem('myKey') ? JSON.parse(localStorage.getItem('myKey')) : {};
+
+function getData() {
+  const itemsArray = {
+  step1: [],
   step2: {
-    yes: ['Meet George','Set timer'], 
-    no: ['Read a book'],
+    yes: [], 
+    no: [],
   },
   question2: {
     yes: [],
@@ -17,66 +21,52 @@ const obj = {
     calendar: [],
     list: [],
   }
+  };
+
+  const step1 = document.getElementById("myUL");
+  for (let i=0; i < step1.childNodes.length; i++) {
+    let li = step1.childNodes[i];
+    itemsArray.step1.unshift(li.childNodes[0].innerText);
+  }
+  const step2yes = document.getElementsByClassName("ul2Yes")[0];
+  for (let i=0; i < step2yes.childNodes.length; i++) {
+    let li = step2yes.childNodes[i];
+    itemsArray.step2.yes.unshift(li.innerText);
+  }
+
+  localStorage.setItem("myKey", JSON.stringify(itemsArray));
 }
-localStorage.setItem("myKey", JSON.stringify(obj));
-// const raw = localStorage.getItem("myKey");
-// const myKey = JSON.parse(raw);
-// let key = "jsj";
-// myKey.step2.push(key);
-// console.log(myKey);
+window.addEventListener("click", getData);
+
+// 1. функція яка бере об'єкт з масивами і по ньому будує веб сторінку з відповідними пунктами "setData(obj)"
 
 function setData() {
   const raw = localStorage.getItem("myKey");
   const myKey1 = JSON.parse(raw).step1;
 
-  for(let i=0; i < myKey1.length; i++) {
+  for (let i=0; i < myKey1.length; i++) {
     let li = document.createElement("li");
     li.innerText = myKey1[i];
     document.getElementById("myUL").appendChild(li);
-    closeBtn();
   }
   const myKey2yes = JSON.parse(raw).step2.yes;
-  for(let i=0; i < myKey2yes.length; i++) {
+  for (let i=0; i < myKey2yes.length; i++) {
     let li = document.createElement("li");
     li.innerText = myKey2yes[i];
     document.getElementsByClassName("ul2Yes")[0].prepend(li);
-    closeBtn();
   }
   const myKey2no = JSON.parse(raw).step2.no;
-  for(let i=0; i < myKey2no.length; i++) {
+  for (let i=0; i < myKey2no.length; i++) {
     let li = document.createElement("li");
     li.innerText = myKey2no[i];
     document.getElementsByClassName("ul2No")[0].prepend(li);
-    closeBtn();
   }
 }
 document.addEventListener("DOMContentLoaded", setData);
 
-// function getData() {
-//   let myKey = {
-//     step1: [],
-//     step2: {
-//       yes: [], 
-//       no: [],
-//     }
-//   };
-//   for {
-//     // let li ...
-//     // myKey.step1.push(li.innerText);
-//   }
-//   // myKey.step1.push(input.value);
-//   // localStorage.setItem("myKey", JSON.stringify(myKey));
-//   // newElement(input.value);
-//   // input.value = "";
 
-//   // const newItem = localStorage.getItem("myKey");
-//   // const data1 = JSON.parse(newItem).step1;
-//   // myKey.step1.push(data1[i]);
-//   console.log(myKey);
-//   // localStorage.setItem("myKey", JSON.stringify(myKey));
-// }
 
-// 1. написати функцію яка бере об'єкт з масивами і по ньому будує веб сторінку з відповідними пунктами "setData(obj)"
+
 // 2. написати функцію яка будує об'єкт "getData()" по тим даним які в тебе є на сторінці
 
 // 3. Після кожної зміни на сторінці необхідно буде викликати функцію getData і зберегти результат в LocalStorage
@@ -92,9 +82,7 @@ function newElement() {
   let inputValue = document.getElementById("myInput").value;
   let t = document.createTextNode(inputValue);
   li.appendChild(t);
-  // myKey.step1.push(inputValue);
-  // console.log(inputValue);
-  
+
   if (inputValue === '') {
     alert("You must write something!");
   } else {
@@ -116,7 +104,7 @@ function newElement() {
       div.style.display = "none";
     }
   }
-  closeBtn();
+  getData();
 } 
 
 // Create a "close" button and append it to each list item
@@ -129,23 +117,23 @@ function closeBtn() {
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
-  }
 // Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-let l;
-for (l = 0; l < close.length; l++) {
-  close[l].onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
+    let close = document.getElementsByClassName("close");
+    let l;
+    for (l = 0; l < close.length; l++) {
+      close[l].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
   }
-}
 }
 
 //__________________________________ 2_Step ________________ 
 //_______________ Creating interactive list ________________
 // сортування задач 
 
-// Копіювання вмісту останього пункту в поле для запитання
+// Копіювання вмісту останнього пункту в поле для запитання
 
 function lastElement() {
   let elem = document.getElementById("myUL").lastElementChild;
